20a6f1e76500aa1d488a116e18d64e84212c92a8
Наталья <1661203@mail.ru>
2023-05-15 20:43:24 +0500

    if_else

---
+++ Example_5_If_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "наталья")
+{
+    Console.WriteLine("Ура, это же сама Наталья!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
